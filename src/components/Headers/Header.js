8df8bfda86d5c590494a242097472de3dffd2dce
sunudk17 <sunudk17@gmail.com>
2021-12-23 12:34:15 +0530

    first

---
+++ src/components/Headers/Header.js
@@ -0,0 +1,163 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+// @material-ui/icons components
+
+import EmojiEvents from "@material-ui/icons/EmojiEvents";
+import GroupAdd from "@material-ui/icons/GroupAdd";
+import InsertChartOutlined from "@material-ui/icons/InsertChartOutlined";
+
+
+// core components
+import CardStats from "components/Cards/CardStats.js";
+
+import componentStyles from "assets/theme/components/header.js";
+
+const useStyles = makeStyles(componentStyles);
+
+const Header = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+  return (
+    <>
+      <div className={classes.header}>
+        <Container
+          maxWidth={false}
+          component={Box}
+          classes={{ root: classes.containerRoot }}
+        >
+          <div>
+            <Grid container>
+              <Grid item xl={3} lg={6} xs={12}>
+                <CardStats
+                  subtitle="User 1"
+                  title="Vehicle 3"
+                  icon={InsertChartOutlined}
+                  color="bgError"
+                  footer={
+                    <>
+                      <Box
+                        component="span"
+                        fontSize=".875rem"
+                        color={theme.palette.success.main}
+                        marginRight=".5rem"
+                        display="flex"
+                        alignItems="center"
+                      >
+                        <Box
+                          
+                          width="1.5rem!important"
+                          height="1.5rem!important"
+                        />{" "}
+                        3
+                      </Box>
+                      <Box component="span" whiteSpace="nowrap">
+                      ------
+                      </Box>
+                    </>
+                  }
+                />
+              </Grid>
+              <Grid item xl={3} lg={6} xs={12}>
+                <CardStats
+                  subtitle="Vehicles"
+                  title="2"
+                 
+                  color="bgWarning"
+                  footer={
+                    <>
+                      <Box
+                        component="span"
+                        fontSize=".875rem"
+                        color={theme.palette.error.main}
+                        marginRight=".5rem"
+                        display="flex"
+                        alignItems="center"
+                      >
+                        <Box
+
+                          width="1.5rem!important"
+                          height="1.5rem!important"
+                        />{" "}
+                        3
+                      </Box>
+                      <Box component="span" whiteSpace="nowrap">
+                        ---
+                      </Box>
+                    </>
+                  }
+                />
+              </Grid>
+              <Grid item xl={3} lg={6} xs={12}>
+                <CardStats
+                  subtitle="Add Vehicle"
+                  title="924"
+                  icon={GroupAdd}
+                  color="bgWarningLight"
+                  footer={
+                    <>
+                      <Box
+                        component="span"
+                        fontSize=".875rem"
+                        color={theme.palette.warning.main}
+                        marginRight=".5rem"
+                        display="flex"
+                        alignItems="center"
+                      >
+                        <Box
+                          
+                          width="1.5rem!important"
+                          height="1.5rem!important"
+                        />{" "}
+                        1
+                      </Box>
+                      <Box component="span" whiteSpace="nowrap">
+                       -----
+                      </Box>
+                    </>
+                  }
+                />
+              </Grid>
+              <Grid item xl={3} lg={6} xs={12}>
+                <CardStats
+                  subtitle="OVERSPEEDING"
+                  title="49,65%"
+                  icon={EmojiEvents}
+                  color="bgInfo"
+                  footer={
+                    <>
+                      <Box
+                        component="span"
+                        fontSize=".875rem"
+                        color={theme.palette.success.main}
+                        marginRight=".5rem"
+                        display="flex"
+                        alignItems="center"
+                      >
+                        <Box
+                         
+                          width="1.5rem!important"
+                          height="1.5rem!important"
+                        />{" "}
+                        1
+                      </Box>
+                      <Box component="span" whiteSpace="nowrap">
+                        ----
+                      </Box>
+                    </>
+                  }
+                />
+              </Grid>
+            </Grid>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default Header;
